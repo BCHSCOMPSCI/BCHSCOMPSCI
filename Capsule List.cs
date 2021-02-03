9faad1a0aca9c492d4ed8f8a84c8bf56fe982e84
BCHSCOMPSCI <valera.liang@grasslands.ab.ca>
2021-02-03 15:27:35 -0700

    Add capsule list

---
+++ Capsule List.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public List<GameObject> CapsuleList = new List<GameObject>();
+public Transform capsule;
+
+void Update()
+{
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+        Spawn();
+    }
+}
+
+public void Spawn()
+{
+    CapsuleList.Add(Instantiate(sphere.gameObject, new vector3(Random.Range(-4.0f, 4.0f), 2, (Random.Range(-4.0f, 4.0f)), Quaternion.identity));
+}
