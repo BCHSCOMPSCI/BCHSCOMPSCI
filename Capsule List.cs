96a7d0d46741b66bc21ec5d35f19c3e1f2e3cf6f
BCHSCOMPSCI <valera.liang@grasslands.ab.ca>
2021-02-03 15:30:15 -0700

    Fixed namespace error

--- Capsule List.cs
+++ Capsule List.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public List<GameObject> CapsuleList = new List<GameObject>();
-public Transform capsule;
-
-void Update()
+public class CapsuleList : MonoBehaviour
 {
-    if (Input.GetKeyDown(KeyCode.Space))
+
+    public List<GameObject> CapsuleList = new List<GameObject>();
+    public Transform capsule;
+
+    void Update()
     {
-        Spawn();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawn();
+        }
     }
-}
 
-public void Spawn()
-{
-    CapsuleList.Add(Instantiate(sphere.gameObject, new vector3(Random.Range(-4.0f, 4.0f), 2, (Random.Range(-4.0f, 4.0f)), Quaternion.identity));
+    public void Spawn()
+    {
+        CapsuleList.Add(Instantiate(sphere.gameObject, new vector3(Random.Range(-4.0f, 4.0f), 2, (Random.Range(-4.0f, 4.0f)), Quaternion.identity));
+    }
 }
