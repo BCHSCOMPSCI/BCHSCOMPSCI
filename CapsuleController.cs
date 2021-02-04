17700885ac4d808bd97b54ac3517a920cf3a8e9b
BCHSCOMPSCI <78448641+BCHSCOMPSCI@users.noreply.github.com>
2021-02-04 15:44:30 -0700

    Add files via upload

--- CapsuleController.cs
+++ CapsuleController.cs
@@ -15,10 +15,31 @@ public class CapsuleController : MonoBehaviour
         {
             Spawn();
         }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            FindSmallScale();
+        }
     }
 
     public void Spawn()
     {
+        float capsuleScale = Random.Range(0.1f, 3.0f);
         CapsuleList.Add(Instantiate(capsule.gameObject, new Vector3(Random.Range(-4.0f, 4.0f), 2, Random.Range(-4.0f, 4.0f)), Quaternion.identity));
+        capsule.transform.localScale = new Vector3(capsuleScale, capsuleScale, capsuleScale);
+    }
+
+    public void FindSmallScale()
+    {
+        var smallCapsuleList = CapsuleList.FindAll(
+            delegate (GameObject g)
+            {
+                return g.GetComponent<Transform>().localScale.y < 1 && g.GetComponent<Transform>().localScale.x < 1 && g.GetComponent<Transform>().localScale.z < 1;
+            });
+
+        foreach(GameObject capsule in smallCapsuleList)
+        {
+            capsule.GetComponent<Renderer>().material.color = new Color(1.0f, 0.0f, 0.0f);
+        }
     }
 }
